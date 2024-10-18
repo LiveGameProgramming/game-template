97e163337c52c8e665a4c639ec3aeabcb6a6fac1
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-18 20:40:16 +0300

    Add draw_arrays functionality to OpenGL commands
    Implemented a new function `draw_arrays` in the Commands class, which uses `glDrawArrays` for rendering. Updated header files and function loaders accordingly, and introduced the `triangles` constant in macros for completeness.

--- code/main.cpp
+++ code/main.cpp
@@ -4,7 +4,13 @@
 #include <opengl/commands.hpp>
 #include <opengl/pipeline.hpp>
 #include <opengl/buffer.hpp>
-#include <opengl/macros.hpp>
+
+struct vec3D
+{
+    float x;
+    float y;
+    float z;
+};
 
 int32_t main()
 {
@@ -20,13 +26,23 @@ int32_t main()
 
     gl::Pipeline::enable(gl::framebuffer_srgb);
 
+    const std::vector<vec3D> vertices
+    {
+        { -0.5f, -0.5f, 0.0f },
+        {  0.5f, -0.5f, 0.0f },
+        {  0.0f,  0.5f, 0.0f }
+    };
+
     gl::Buffer vertex_buffer;
     vertex_buffer.create();
+    vertex_buffer.data(core::BufferData::create(vertices));
 
     while (core::WindowManager::instance().is_active())
     {
         gl::Commands::clear(1.0f, 0.5f, 0.0f);
         gl::Commands::clear(gl::color_buffer_bit);
+
+        gl::Commands::draw_arrays(gl::triangles, vertices.size());
 
         core::WindowManager::instance().update();
     }
