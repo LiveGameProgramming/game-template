3b05031fba42b5c3e6c8f6e66e443320fa0ae866
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-21 23:07:54 +0300

    Update vertex handling and cleanup in main program
    Replaced struct vec3D with math::vec3 from math library to standardize vector usage. Added vertex array creation, binding, and destruction for proper OpenGL state management. Removed unnecessary includes and streamlined the code.

--- code/main.cpp
+++ code/main.cpp
@@ -1,16 +1,11 @@
 #include <core/window_manager.hpp>
 
 #include <opengl/functions_loader.hpp>
+#include <opengl/vertex_array.hpp>
 #include <opengl/commands.hpp>
 #include <opengl/pipeline.hpp>
-#include <opengl/buffer.hpp>
 
-struct vec3D
-{
-    float x;
-    float y;
-    float z;
-};
+#include <math/vec3.hpp>
 
 int32_t main()
 {
@@ -26,7 +21,7 @@ int32_t main()
 
     gl::Pipeline::enable(gl::framebuffer_srgb);
 
-    const std::vector<vec3D> vertices
+    const std::vector<math::vec3> vertices
     {
         { -0.5f, -0.5f, 0.0f },
         {  0.5f, -0.5f, 0.0f },
@@ -37,10 +32,15 @@ int32_t main()
     vertex_buffer.create();
     vertex_buffer.data(core::BufferData::create(vertices));
 
+    gl::VertexArray vertex_array;
+    vertex_array.create();
+
     while (core::WindowManager::instance().is_active())
     {
         gl::Commands::clear(1.0f, 0.5f, 0.0f);
         gl::Commands::clear(gl::color_buffer_bit);
+
+        vertex_array.bind();
 
         gl::Commands::draw_arrays(gl::triangles, vertices.size());
 
@@ -48,6 +48,7 @@ int32_t main()
     }
 
     vertex_buffer.destroy();
+    vertex_array.destroy();
 
     core::WindowManager::instance().destroy();
 
