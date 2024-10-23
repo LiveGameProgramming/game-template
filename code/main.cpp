986a5968b4e0687d0816e1a931ea4415f85693b2
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-23 23:16:48 +0300

    Add shader handling to main and update shader color
    Included core file handling to load shaders from files and integrated shader binding and cleanup in the main.cpp. Modified default fragment shader color output to a new shade.

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,12 @@
 #include <core/window_manager.hpp>
+#include <core/file.hpp>
 
 #include <opengl/functions_loader.hpp>
-#include <opengl/vertex_array.hpp>
 #include <opengl/commands.hpp>
 #include <opengl/pipeline.hpp>
+
+#include <opengl/vertex_array.hpp>
+#include <opengl/shader.hpp>
 
 #include <math/vec3.hpp>
 
@@ -21,6 +24,27 @@ int32_t main()
 
     gl::Pipeline::enable(gl::framebuffer_srgb);
     gl::Pipeline::enable(gl::multisample);
+
+    #pragma region Shaders
+
+    gl::ShaderStage vertex_stage { gl::vertex_stage };
+    vertex_stage.create();
+    vertex_stage.source(core::File::read("default_shader.vert", std::ios::binary));
+
+    gl::ShaderStage fragment_stage { gl::fragment_stage };
+    fragment_stage.create();
+    fragment_stage.source(core::File::read("default_shader.frag", std::ios::binary));
+
+    gl::Shader default_shader;
+    default_shader.create();
+    default_shader.attach(vertex_stage);
+    default_shader.attach(fragment_stage);
+    default_shader.link();
+
+    vertex_stage.destroy();
+    fragment_stage.destroy();
+
+    #pragma endregion
 
     const std::vector<math::vec3> vertices
     {
@@ -55,6 +79,8 @@ int32_t main()
         gl::Commands::clear(1.0f, 0.5f, 0.0f);
         gl::Commands::clear(gl::color_buffer_bit);
 
+        default_shader.bind();
+
         vertex_array.bind();
 
         gl::Commands::draw_indexed(gl::triangles, static_cast<int32_t>(indices.size()));
@@ -65,6 +91,8 @@ int32_t main()
     index_buffer.destroy();
     vertex_buffer.destroy();
     vertex_array.destroy();
+
+    default_shader.destroy();
 
     core::WindowManager::instance().destroy();
 
