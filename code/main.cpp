21fa50b4b86b6c63aa0035f720ada81459345102
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-30 10:43:26 +0200

    Refactor: Replace core namespace with base namespace
    Switched from the core namespace to the base namespace for shader stages, buffers, math functions, and window management. This change standardizes the codebase and aligns all references under the base namespace.

--- code/main.cpp
+++ code/main.cpp
@@ -32,11 +32,11 @@ int32_t main()
 
     opengl::ShaderStage vertex_stage { opengl::vertex_stage };
     vertex_stage.create();
-    vertex_stage.source(core::File::read("default_shader.vert", std::ios::binary));
+    vertex_stage.source(base::File::read("default_shader.vert", std::ios::binary));
 
     opengl::ShaderStage fragment_stage { opengl::fragment_stage };
     fragment_stage.create();
-    fragment_stage.source(core::File::read("default_shader.frag", std::ios::binary));
+    fragment_stage.source(base::File::read("default_shader.frag", std::ios::binary));
 
     opengl::Shader default_shader;
     default_shader.create();
@@ -49,7 +49,7 @@ int32_t main()
 
     #pragma endregion
 
-    const std::vector<core::vec3> vertices
+    const std::vector<base::vec3> vertices
     {
         { -0.5f,  0.5f, 0.0f },
         {  0.5f,  0.5f, 0.0f },
@@ -65,24 +65,24 @@ int32_t main()
 
     opengl::Buffer vertex_buffer;
     vertex_buffer.create();
-    vertex_buffer.data(core::buffer::data::create(vertices));
+    vertex_buffer.data(base::buffer::data::create(vertices));
 
     opengl::Buffer index_buffer;
     index_buffer.create();
-    index_buffer.data(core::buffer::data::create(indices));
+    index_buffer.data(base::buffer::data::create(indices));
 
     opengl::VertexArray vertex_array;
     vertex_array.create();
-    vertex_array.attach_vertices(vertex_buffer, sizeof(core::vec3));
+    vertex_array.attach_vertices(vertex_buffer, sizeof(base::vec3));
     vertex_array.attach_indices(index_buffer);
     vertex_array.attribute({ 0, 3, opengl::type_float });
 
     const float aspect_ratio = static_cast<float>(base::WindowManager::instance().width()) /
                                static_cast<float>(base::WindowManager::instance().height());
-    core::mat4 proj;
+    base::mat4 proj;
     proj.perspective(60.0f, aspect_ratio, 0.1f, 100.0f);
 
-    core::mat4 view;
+    base::mat4 view;
     view.look_at({ 0.0f, 0.0f, 5.0f }, { }, { 0.0f, 1.0f, 0.0f });
 
     #pragma region Uniform Buffers
@@ -92,7 +92,7 @@ int32_t main()
     opengl::Buffer camera_ubo;
     camera_ubo.create();
     camera_ubo.bind();
-    camera_ubo.data(core::buffer::data::create(camera_data));
+    camera_ubo.data(base::buffer::data::create(camera_data));
 
     #pragma endregion
 
@@ -104,12 +104,12 @@ int32_t main()
         time.update();
 
         const float r = 5.0f;
-        const float x = core::math::sin(base::Time::total_time()) * -r;
-        const float z = core::math::cos(base::Time::total_time()) *  r;
+        const float x = base::math::sin(base::Time::total_time()) * -r;
+        const float z = base::math::cos(base::Time::total_time()) *  r;
 
         view.look_at({ x, 0.0f, z }, { }, { 0.0f, 1.0f, 0.0f });
 
-        camera_ubo.sub_data(core::buffer::data::create(&view));
+        camera_ubo.sub_data(base::buffer::data::create(&view));
 
         opengl::Commands::clear(1.0f, 0.5f, 0.0f);
         opengl::Commands::clear(opengl::color_buffer_bit);
@@ -118,7 +118,7 @@ int32_t main()
 
         vertex_array.bind();
 
-        core::mat4 model;
+        base::mat4 model;
         model.identity();
 
         default_shader.push_mat4(0, model);
