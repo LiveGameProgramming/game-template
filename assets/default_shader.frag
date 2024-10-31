06aa97977c0e76fe32e59a68ee62b15a912e69e1
iamdramaticleo <iamdramaticleo@gmail.com>
2024-10-31 19:11:31 +0200

    Add face template to geometry and new RGB and Triangle structs
    Updated the geometry template to include a face type, replacing indices with faces. Added a new RGB struct for color representation. Introduced a Triangle struct to define a primitive with three vertex indices.

--- assets/default_shader.frag
+++ assets/default_shader.frag
@@ -1,8 +1,14 @@
 #version 460
 
+layout (binding = 1, std140) uniform u_material
+{
+    vec3 diffuse;
+}
+material;
+
 layout (location = 0) out vec4 out_color;
 
 void main()
 {
-    out_color = vec4(0.0, 0.5, 1.0, 1.0);
+    out_color = vec4(material.diffuse, 1.0);
 }
--- code/main.cpp
+++ code/main.cpp
@@ -5,10 +5,12 @@
 #include <base/file.hpp>
 #include <base/mat4.hpp>
 #include <base/time.hpp>
+#include <base/rgb.hpp>
 
 #include <opengl/vertex_array.hpp>
 #include <opengl/commands.hpp>
 #include <opengl/shader.hpp>
+//#include <opengl/pipeline.hpp>
 
 #include <math/functions.hpp>
 
@@ -22,6 +24,8 @@ int32_t main()
     base::WindowManager::instance().display();
 
     base::GraphicsModule::init();
+
+    //opengl::Pipeline::enable_lines_mode();
 
     #pragma region Shaders
 
@@ -47,7 +51,7 @@ int32_t main()
 
     #pragma region Plane
 
-    auto [plane_vertices, plane_indices] = editor::base::Primitives::create_plane(5.0f, 5.0f);
+    auto [plane_vertices, plane_faces] = editor::base::Primitives::create_plane(5.0f, 5.0f);
 
     opengl::Buffer plane_vertex_buffer;
     plane_vertex_buffer.create();
@@ -55,7 +59,7 @@ int32_t main()
 
     opengl::Buffer plane_index_buffer;
     plane_index_buffer.create();
-    plane_index_buffer.data(buffers::data::create(plane_indices));
+    plane_index_buffer.data(buffers::data::create(plane_faces));
 
     opengl::VertexArray plane_vertex_array;
     plane_vertex_array.create();
@@ -64,23 +68,49 @@ int32_t main()
     plane_vertex_array.attribute({ 0, 3, opengl::type_float });
 
     #pragma endregion
+    #pragma region Box
+
+    auto [box_vertices, box_faces] = editor::base::Primitives::create_box();
+
+    opengl::Buffer box_vertex_buffer;
+    box_vertex_buffer.create();
+    box_vertex_buffer.data(buffers::data::create(box_vertices));
+
+    opengl::Buffer box_index_buffer;
+    box_index_buffer.create();
+    box_index_buffer.data(buffers::data::create(box_faces));
+
+    opengl::VertexArray box_vertex_array;
+    box_vertex_array.create();
+    box_vertex_array.attach_vertices(box_vertex_buffer, sizeof(editor::base::vertex));
+    box_vertex_array.attach_indices(box_index_buffer);
+    box_vertex_array.attribute({ 0, 3, opengl::type_float });
+
+    #pragma endregion
+
+    std::vector<base::mat4>  camera_data { 2 };
+
+              base::vec3 camera_position { 0.0f, 2.5, 0.0f };
+    constexpr base::rgb  plane_color     { 0.0f, 0.5f, 1.0f };
+    constexpr base::rgb  box_color       { 0.7f, 0.5f, 0.8f };
 
     const float aspect_ratio = static_cast<float>(base::WindowManager::instance().width()) /
                                static_cast<float>(base::WindowManager::instance().height());
-    base::mat4 proj;
-    proj.perspective(60.0f, aspect_ratio, 0.1f, 100.0f);
 
-    base::mat4 view;
-    view.look_at({ 0.0f, 2.5f, 5.0f }, { });
+    camera_data[0].look_at({ 0.0f, 2.5f, 5.0f }, { });
+    camera_data[1].perspective(60.0f, aspect_ratio, 0.1f, 100.0f);
 
     #pragma region Uniform Buffers
-
-    const std::vector camera_data { view, proj };
 
     opengl::Buffer camera_ubo;
     camera_ubo.create();
     camera_ubo.bind();
     camera_ubo.data(buffers::data::create(camera_data));
+
+    opengl::Buffer material_ubo;
+    material_ubo.create();
+    material_ubo.bind(buffers::location::material);
+    material_ubo.data(buffers::data::create(&plane_color));
 
     #pragma endregion
 
@@ -91,38 +121,63 @@ int32_t main()
     {
         time.update();
 
-        constexpr float camera_speed = 0.25f;
+        constexpr float camera_speed  = 0.25f;
+        constexpr float camera_radius = 5.00f;
 
-        const float r = 5.0f;
-        const float x = math::sin(base::Time::total_time() * camera_speed) * -r;
-        const float z = math::cos(base::Time::total_time() * camera_speed) *  r;
+        camera_position.x = math::sin(base::Time::total_time() * camera_speed) * -camera_radius;
+        camera_position.z = math::cos(base::Time::total_time() * camera_speed) *  camera_radius;
 
-        view.look_at({ x, 2.5f, z }, { });
-
-        camera_ubo.sub_data(buffers::data::create(&view));
+        camera_data[0].look_at(camera_position, { });
+        camera_ubo.sub_data(buffers::data::create(&camera_data[0]));
 
         opengl::Commands::clear(1.0f, 0.5f, 0.0f);
         opengl::Commands::clear(opengl::color_buffer);
 
         default_shader.bind();
 
+        #pragma region Plane
+
+        base::mat4 plane_model_matrix;
+        plane_model_matrix.identity();
+        default_shader.push_mat4(0, plane_model_matrix);
+
+        material_ubo.sub_data(buffers::data::create(&plane_color));
+
         plane_vertex_array.bind();
+        opengl::Commands::draw_indexed(opengl::triangles, static_cast<int32_t>(plane_faces.size()) * primitives::triangle::elements);
 
-        base::mat4 model;
-        model.identity();
+        #pragma endregion
+        #pragma region Box
 
-        default_shader.push_mat4(0, model);
+        base::mat4 box_model_matrix;
+        box_model_matrix.identity();
+        box_model_matrix.translate({ 0.0f, 1.0f, 1.0f });
+        default_shader.push_mat4(0, box_model_matrix);
 
-        opengl::Commands::draw_indexed(opengl::triangles, static_cast<int32_t>(plane_indices.size()));
+        material_ubo.sub_data(buffers::data::create(&box_color));
+
+        box_vertex_array.bind();
+        opengl::Commands::draw_indexed(opengl::triangles, static_cast<int32_t>(box_faces.size()) * primitives::triangle::elements);
+
+        #pragma endregion
 
         base::WindowManager::instance().update();
     }
+
+    #pragma region Primitives
 
     plane_index_buffer.destroy();
     plane_vertex_buffer.destroy();
     plane_vertex_array.destroy();
 
+    box_index_buffer.destroy();
+    box_vertex_buffer.destroy();
+    box_vertex_array.destroy();
+
+    #pragma endregion
+
     camera_ubo.destroy();
+    material_ubo.destroy();
 
     default_shader.destroy();
 
